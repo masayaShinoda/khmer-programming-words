d746d15d28bebfd454aced19bb4b394a05877c84
Masaya Shida <masayashida@gmail.com>
2023-07-16 20:32:13 +0700

    add files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="section_top">
+    <a href="#">បន្ថែមពាក្យថ្មី</a>
+</section>
+
+<section class="section_hero">
+    <div>
+        <img src="/SVG/decorative-hero-section.svg" width="150" height="150" alt="decorative icon">
+    </div>
+    <div>
+        <h1>ពាក្យបច្ចេកទេសបកប្រែជាភាសាខ្មែរ</h1>
+        <span lang="en">A collection of technical words commonly used in programming, translated to Khmer.</span>
+    </div>
+</section>
+
+<section class="section_alpha_nav">
+    <nav id="alpha_nav">
+        <a href="#a">A</a>
+    </nav>
+    <noscript>JavaScript is required to use this navigation feature.</noscript>
+</section>
+
+<section class="section_main">
+    <dl id="definition_list">
+        <dt>Array - អារេ</dt>
+        <dd>lorem ipsum</dd>
+
+        <dt>Binary - គោលពីរ</dt>
+        <dd>Lorem ipsum</dd>
+
+        <dt>Break - បញ្ចប់</dt>
+        <dd>Lorem ipsum</dd>
+
+
+        <dt>Conditional - តាមលក្ខខណ្ឌ</dt>
+        <dd>Lorem ipsum</dd>
+    </dl>
+</section>
