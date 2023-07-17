7cef7789c08d2cbbd8ff743723cfccab1d01dc4f
Masaya Shida <masayashida@gmail.com>
2023-07-17 19:26:47 +0700

    update styling

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,12 +1,12 @@
 <section class="section_hero">
 	<div>
-		<img src="/SVG/decorative-hero-section.svg" width="150" height="150" alt="decorative icon" />
+		<img src="/SVG/decorative-hero-section.svg" width="128" height="128" alt="decorative icon" />
 	</div>
 	<div>
 		<h1>ពាក្យបច្ចេកទេសបកប្រែជាភាសាខ្មែរ</h1>
-		<span lang="en"
-			>An <span style="font-weight: 600;">opinionated</span> collection of technical words commonly used
-			in programming, translated into Khmer.</span
-		>
+		<p lang="en">
+			An <span style="font-weight: 600;">opinionated</span> collection of technical words commonly used
+			in programming, translated into Khmer.
+		</p>
 	</div>
 </section>
