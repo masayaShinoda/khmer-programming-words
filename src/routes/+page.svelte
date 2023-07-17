4105dde26aebde1a2d8ac22718d6c329345c7972
Masaya Shida <masayashida@gmail.com>
2023-07-17 18:43:52 +0700

    nav working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,32 @@
 	import SEOHead from '$lib/SEOHead.svelte';
 	import SectionTop from '$lib/SectionTop.svelte';
 	import Hero from '$lib/Hero.svelte';
-	import type { PageData } from './$types';
+	import { dictionary } from '$lib/content/dictionary';
 
-	export let data: PageData;
+	type DictionaryKey = keyof typeof dictionary;
 
-	let dictionary_keys = Object.keys(data.content);
+	const dictionary_keys = Object.keys(dictionary) as DictionaryKey[];
 
-	let first_char_array = dictionary_keys.map((item) => {
+	const first_char_array = dictionary_keys.map((item) => {
 		if (item[0] !== ':') {
 			return item[0];
 		} else return '';
 	});
 
-	export function removeDuplicates(arr: string[]) {
+	// determine which <dt> gets to have the alphabetical ID tag for navigation
+	const anchor_keys = dictionary_keys
+		.map((key, index) => {
+			if (index !== 0) {
+				if (key[0].toUpperCase() !== dictionary_keys[index - 1][0].toUpperCase()) {
+					return key;
+				} else return '';
+			} else if (index === 0) {
+				return key;
+			}
+		})
+		.filter((item) => item !== '');
+
+	function removeDuplicates(arr: string[]) {
 		return [...new Set(arr)];
 	}
 </script>
@@ -39,8 +52,13 @@
 <section class="section_main">
 	<dl id="definition_list">
 		{#each dictionary_keys as item}
-			<dt>{item}</dt>
-			{#each data.content[`${item}`] as definition}
+			<!-- if its the first occurence of an alphabetical character, add id -->
+			{#if anchor_keys.includes(item)}
+				<dt id={item[0].toUpperCase()}>{item}</dt>
+			{:else}
+				<dt>{item}</dt>
+			{/if}
+			{#each dictionary[item] as definition}
 				<dd>{definition}</dd>
 			{/each}
 		{/each}
--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-export async function load({ fetch }) {
-  const response = await fetch("src/content/dictionary.json")
-  const content = await response.json()
-
-  return { content }
-}
-
-// pre-render method
-export const prerender = true; 
